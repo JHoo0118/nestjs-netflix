c54d21e13942f3b787f89c435bc4ebdd03bee68c
JHoo0118 <kik3078@gmail.com>
2024-10-03 21:40:32 +0900

    회원가입

--- src/app.module.ts
+++ src/app.module.ts
@@ -11,6 +11,9 @@ import { DirectorModule } from './director/director.module';
 import { Director } from './director/entity/director.entity';
 import { GenreModule } from './genre/genre.module';
 import { Genre } from './genre/entity/genre.entity';
+import { AuthModule } from './auth/auth.module';
+import { UserModule } from './user/user.module';
+import { User } from './user/entity/user.entity';
 
 @Module({
   imports: [
@@ -24,6 +27,7 @@ import { Genre } from './genre/entity/genre.entity';
         DB_USERNAME: Joi.string().required(),
         DB_PASSWORD: Joi.string().required(),
         DB_DATABASE: Joi.string().required(),
+        HASH_ROUNDS: Joi.number().required(),
       }),
     }),
     TypeOrmModule.forRootAsync({
@@ -34,7 +38,7 @@ import { Genre } from './genre/entity/genre.entity';
         username: configService.get<string>('DB_USERNAME'),
         password: configService.get<string>('DB_PASSWORD'),
         database: configService.get<string>('DB_DATABASE'),
-        entities: [Movie, MovieDetail, Director, Genre],
+        entities: [Movie, MovieDetail, Director, Genre, User],
         synchronize: true,
       }),
       inject: [ConfigService],
@@ -42,6 +46,8 @@ import { Genre } from './genre/entity/genre.entity';
     MovieModule,
     DirectorModule,
     GenreModule,
+    AuthModule,
+    UserModule,
   ],
   controllers: [AppController],
   providers: [AppService],
---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,41 @@
+import { Controller, Post, Headers } from '@nestjs/common';
+import { AuthService } from './auth.service';
+
+@Controller('auth')
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+
+  @Post('register')
+  /// authorization: Basic $token
+  registerUser(@Headers('authorization') token: string) {
+    return this.authService.register(token);
+  }
+
+  // @Post('login')
+  // /// authorization: Basic $token
+  // loginUser(@Headers('authorization') token: string) {
+  //   return this.authService.login(token);
+  // }
+
+  // @Post('token/access')
+  // async rotateAccessToken(@Request() req) {
+  //   return {
+  //     accessToken: await this.authService.issueToken(req.user, false),
+  //   };
+  // }
+
+  // @UseGuards(LocalAuthGuard)
+  // @Post('login/passport')
+  // async loginUserPassport(@Request() req) {
+  //   return {
+  //     refreshToken: await this.authService.issueToken(req.user, true),
+  //     accessToken: await this.authService.issueToken(req.user, false),
+  //   };
+  // }
+
+  // @UseGuards(JwtAuthGuard)
+  // @Get('private')
+  // async private(@Request() req) {
+  //   return req.user;
+  // }
+}
