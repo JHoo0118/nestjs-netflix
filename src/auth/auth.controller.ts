e9ad91e17b1ef12a38070ddd197107ae0bb0a073
JHoo0118 <kik3078@gmail.com>
2024-10-03 22:06:55 +0900

    토큰 관리

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,14 @@
-import { Controller, Post, Headers, Request } from '@nestjs/common';
+import {
+  Controller,
+  Post,
+  Headers,
+  Request,
+  UseGuards,
+  Get,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
+import { LocalAuthGuard } from './strategy/local.strategy';
+import { JwtAuthGuard } from './strategy/jwt.strategy';
 
 @Controller('auth')
 export class AuthController {
@@ -17,25 +26,33 @@ export class AuthController {
     return this.authService.login(token);
   }
 
-  @Post('token/access')
-  async rotateAccessToken(@Request() req) {
-    return {
-      accessToken: await this.authService.issueToken(req.user, false),
-    };
-  }
-
-  // @UseGuards(LocalAuthGuard)
-  // @Post('login/passport')
-  // async loginUserPassport(@Request() req) {
+  // @Post('token/access')
+  // async rotateAccessToken(@Request() req) {
   //   return {
-  //     refreshToken: await this.authService.issueToken(req.user, true),
   //     accessToken: await this.authService.issueToken(req.user, false),
   //   };
   // }
 
-  // @UseGuards(JwtAuthGuard)
-  // @Get('private')
-  // async private(@Request() req) {
-  //   return req.user;
-  // }
+  @Post('token/access')
+  async rotateAccessToken(@Headers('authorization') token: string) {
+    const payload = await this.authService.parseBearerToken(token, true);
+    return {
+      accessToken: await this.authService.issueToken(payload, false),
+    };
+  }
+
+  @UseGuards(LocalAuthGuard)
+  @Post('login/passport')
+  async loginUserPassport(@Request() req) {
+    return {
+      refreshToken: await this.authService.issueToken(req.user, true),
+      accessToken: await this.authService.issueToken(req.user, false),
+    };
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Get('private')
+  async private(@Request() req) {
+    return req.user;
+  }
 }
